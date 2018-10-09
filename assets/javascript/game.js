4ab1d6dcd3c5881b0f4d8e596d95d8181de5de71
Git <brittanymariealdaco@gmail.com>
2018-10-09 10:06:17 -0700

    r

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,16 @@
-<head>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-</head>
+
+
+var firstNumber = "";
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
+
+var firstNumber = document.getElementById("#firstnumber")
+ 
+var firstNumber = Math.floor(Math.random()* 50);
+
+var red = document.getElementById("#red");
+
+
+
 
