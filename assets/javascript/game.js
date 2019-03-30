55d11fdd2c9bc97fb6298aa538cb56239a4ce4f2
brittanymariea <brittanymariealdaco@gmail.com>
2019-03-30 13:08:58 -0700

    functioning version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,63 @@
+var randomNumber = Math.floor(Math.random()* 100) + 1;
 
+var total = 0;
 
-var firstNumber = "";
 var wins = 0;
+
 var losses = 0;
-var totalScore = 0;
 
-var firstNumber = document.getElementById("#firstnumber")
- 
-var firstNumber = Math.floor(Math.random()* 50);
+$("#numberToGuess").text(randomNumber);
+$("#total").text(total);
+$("#wins").text(wins);
+$("#losses").text(losses);
 
-var red = document.getElementById("#red");
+$(".rupees").each(function(){
+    var random = Math.floor(Math.random()* 10) + 1;
+    $(this).attr("value", random)
+    console.log(random)
+})
+
+// total equals 0 so it starts at 0 therefore total = total + value @ start is total = 0+1
+$(".rupees").on("click", function () {
+    var value = parseInt(this.value);
+    total = total + value
+    $("#total").text(total);
+
+    if (total === randomNumber) {
+        console.log("you win");
+    } else if (total > randomNumber) {
+        console.log("you lose");
+    }
+
+    if (total === randomNumber) {
+        console.log("wins")
+        wins++;
+        $("#wins").text(wins); 
+        resetGame()  //invocking the function resetGame
+    } else if (total > randomNumber) {
+        console.log("lose")
+        losses++;
+        $("#losses").text(losses);
+        resetGame()
+    }
+});
+
+function resetGame() {
+    var randomNumber = Math.floor(Math.random()* 100) + 1;
+    var total = 0;
+
+    $("#numberToGuess").text(randomNumber);
+    $("#total").text(total);
+   
+    $(".rupees").each(function(){
+        var random = Math.floor(Math.random()* 10) + 1;
+        $(this).attr("value", random)
+        console.log(random)
+    })
+    
+}
+
+
 
 
 
